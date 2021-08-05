1d836f133d1ee30d4e6634eeb556f789ca2c300b
Jorge Ricardo Hernández Sabino <40583956+HunterKTF@users.noreply.github.com>
2021-08-05 11:25:54 -0500

    controller B test 2

--- test/system/mamdani/mamdani_B_test.exs
+++ test/system/mamdani/mamdani_B_test.exs
@@ -81,55 +81,15 @@ defmodule MandaniBTest do
       Variable.new(tag: "water_flow_rate_delta_vis", fuzzy_sets: fuzzy_sets, type: :consequent, range: [-2000, 2000])
 
     # Raw Material Flow Rate Delta Controller
-    r1 = fn [at1, at2, con] -> (at1 ~> "mb" &&& at2 ~> "mb") >>> con ~> "mb"
+    r1 = fn [at1, con] -> (at1 ~> "mb" &&& at1 ~> "mb") >>> con ~> "mb"
     end
-    r2 = fn [at1, at2, con] -> (at1 ~> "mb" &&& at2 ~> "ms") >>> con ~> "mb"
+    r2 = fn [at1, con] -> (at1 ~> "ms" &&& at1 ~> "ms") >>> con ~> "mb"
     end
-    r3 = fn [at1, at2, con] -> (at1 ~> "mb" &&& at2 ~> "ze") >>> con ~> "mb"
+    r3 = fn [at1, con] -> (at1 ~> "ze" &&& at1 ~> "ze") >>> con ~> "mb"
     end
-    r4 = fn [at1, at2, con] -> (at1 ~> "mb" &&& at2 ~> "ps") >>> con ~> "ms"
+    r4 = fn [at1, con] -> (at1 ~> "ps" &&& at1 ~> "ps") >>> con ~> "ms"
     end
-    r5 = fn [at1, at2, con] -> (at1 ~> "mb" &&& at2 ~> "pb") >>> con ~> "ms"
-    end
-    r6 = fn [at1, at2, con] -> (at1 ~> "ms" &&& at2 ~> "mb") >>> con ~> "mb"
-    end
-    r7 = fn [at1, at2, con] -> (at1 ~> "ms" &&& at2 ~> "ms") >>> con ~> "ms"
-    end
-    r8 = fn [at1, at2, con] -> (at1 ~> "ms" &&& at2 ~> "ze") >>> con ~> "ms"
-    end
-    r9 = fn [at1, at2, con] -> (at1 ~> "ms" &&& at2 ~> "ps") >>> con ~> "ze"
-    end
-    r10 = fn [at1, at2, con] -> (at1 ~> "ms" &&& at2 ~> "pb") >>> con ~> "ms"
-    end
-    r11 = fn [at1, at2, con] -> (at1 ~> "ze" &&& at2 ~> "mb") >>> con ~> "ms"
-    end
-    r12 = fn [at1, at2, con] -> (at1 ~> "ze" &&& at2 ~> "ms") >>> con ~> "ze"
-    end
-    r13 = fn [at1, at2, con] -> (at1 ~> "ze" &&& at2 ~> "ze") >>> con ~> "ze"
-    end
-    r14 = fn [at1, at2, con] -> (at1 ~> "ze" &&& at2 ~> "ps") >>> con ~> "ze"
-    end
-    r15 = fn [at1, at2, con] -> (at1 ~> "ze" &&& at2 ~> "pb") >>> con ~> "ps"
-    end
-    r16 = fn [at1, at2, con] -> (at1 ~> "ps" &&& at2 ~> "mb") >>> con ~> "ps"
-    end
-    r17 = fn [at1, at2, con] -> (at1 ~> "ps" &&& at2 ~> "ms") >>> con ~> "ze"
-    end
-    r18 = fn [at1, at2, con] -> (at1 ~> "ps" &&& at2 ~> "ze") >>> con ~> "ps"
-    end
-    r19 = fn [at1, at2, con] -> (at1 ~> "ps" &&& at2 ~> "ps") >>> con ~> "ps"
-    end
-    r20 = fn [at1, at2, con] -> (at1 ~> "ps" &&& at2 ~> "pb") >>> con ~> "pb"
-    end
-    r21 = fn [at1, at2, con] -> (at1 ~> "pb" &&& at2 ~> "mb") >>> con ~> "ps"
-    end
-    r22 = fn [at1, at2, con] -> (at1 ~> "pb" &&& at2 ~> "ms") >>> con ~> "ps"
-    end
-    r23 = fn [at1, at2, con] -> (at1 ~> "pb" &&& at2 ~> "ze") >>> con ~> "pb"
-    end
-    r24 = fn [at1, at2, con] -> (at1 ~> "pb" &&& at2 ~> "ps") >>> con ~> "pb"
-    end
-    r25 = fn [at1, at2, con] -> (at1 ~> "pb" &&& at2 ~> "pb") >>> con ~> "pb"
+    r5 = fn [at1, con] -> (at1 ~> "pb" &&& at1 ~> "pb") >>> con ~> "ms"
     end
 
     # Raw Material Residual Error
@@ -138,32 +98,8 @@ defmodule MandaniBTest do
     rule3 = Rule.new(statement: r3, consequent: raw_material_flow_rate_delta_res.tag, antecedent: [residual_error.tag])
     rule4 = Rule.new(statement: r4, consequent: raw_material_flow_rate_delta_res.tag, antecedent: [residual_error.tag])
     rule5 = Rule.new(statement: r5, consequent: raw_material_flow_rate_delta_res.tag, antecedent: [residual_error.tag])
-    rule6 = Rule.new(statement: r6, consequent: raw_material_flow_rate_delta_res.tag, antecedent: [residual_error.tag])
-    rule7 = Rule.new(statement: r7, consequent: raw_material_flow_rate_delta_res.tag, antecedent: [residual_error.tag])
-    rule8 = Rule.new(statement: r8, consequent: raw_material_flow_rate_delta_res.tag, antecedent: [residual_error.tag])
-    rule9 = Rule.new(statement: r9, consequent: raw_material_flow_rate_delta_res.tag, antecedent: [residual_error.tag])
-    rule10 = Rule.new(statement: r10, consequent: raw_material_flow_rate_delta_res.tag, antecedent: [residual_error.tag])
-    rule11 = Rule.new(statement: r11, consequent: raw_material_flow_rate_delta_res.tag, antecedent: [residual_error.tag])
-    rule12 = Rule.new(statement: r12, consequent: raw_material_flow_rate_delta_res.tag, antecedent: [residual_error.tag])
-    rule13 = Rule.new(statement: r13, consequent: raw_material_flow_rate_delta_res.tag, antecedent: [residual_error.tag])
-    rule14 = Rule.new(statement: r14, consequent: raw_material_flow_rate_delta_res.tag, antecedent: [residual_error.tag])
-    rule15 = Rule.new(statement: r15, consequent: raw_material_flow_rate_delta_res.tag, antecedent: [residual_error.tag])
-    rule16 = Rule.new(statement: r16, consequent: raw_material_flow_rate_delta_res.tag, antecedent: [residual_error.tag])
-    rule17 = Rule.new(statement: r17, consequent: raw_material_flow_rate_delta_res.tag, antecedent: [residual_error.tag])
-    rule18 = Rule.new(statement: r18, consequent: raw_material_flow_rate_delta_res.tag, antecedent: [residual_error.tag])
-    rule19 = Rule.new(statement: r19, consequent: raw_material_flow_rate_delta_res.tag, antecedent: [residual_error.tag])
-    rule20 = Rule.new(statement: r20, consequent: raw_material_flow_rate_delta_res.tag, antecedent: [residual_error.tag])
-    rule21 = Rule.new(statement: r21, consequent: raw_material_flow_rate_delta_res.tag, antecedent: [residual_error.tag])
-    rule22 = Rule.new(statement: r22, consequent: raw_material_flow_rate_delta_res.tag, antecedent: [residual_error.tag])
-    rule23 = Rule.new(statement: r23, consequent: raw_material_flow_rate_delta_res.tag, antecedent: [residual_error.tag])
-    rule24 = Rule.new(statement: r24, consequent: raw_material_flow_rate_delta_res.tag, antecedent: [residual_error.tag])
-    rule25 = Rule.new(statement: r25, consequent: raw_material_flow_rate_delta_res.tag, antecedent: [residual_error.tag])
 
-    rules = [rule1,rule2,rule3,rule4,rule5,
-            rule6,rule7,rule8,rule9,rule10,
-            rule11,rule12,rule13,rule14,rule15,
-            rule16,rule17,rule18,rule19,rule20,
-            rule21,rule22,rule23,rule24,rule25]
+    rules = [rule1,rule2,rule3,rule4,rule5]
 
     {:ok, srm_res_pid} =
       System.start_link(antecedent: [residual_error], consequent: raw_material_flow_rate_delta_res, rules: rules)
@@ -175,87 +111,23 @@ defmodule MandaniBTest do
     rule3 = Rule.new(statement: r3, consequent: raw_material_flow_rate_delta_vis.tag, antecedent: [viscosity_error.tag])
     rule4 = Rule.new(statement: r4, consequent: raw_material_flow_rate_delta_vis.tag, antecedent: [viscosity_error.tag])
     rule5 = Rule.new(statement: r5, consequent: raw_material_flow_rate_delta_vis.tag, antecedent: [viscosity_error.tag])
-    rule6 = Rule.new(statement: r6, consequent: raw_material_flow_rate_delta_vis.tag, antecedent: [viscosity_error.tag])
-    rule7 = Rule.new(statement: r7, consequent: raw_material_flow_rate_delta_vis.tag, antecedent: [viscosity_error.tag])
-    rule8 = Rule.new(statement: r8, consequent: raw_material_flow_rate_delta_vis.tag, antecedent: [viscosity_error.tag])
-    rule9 = Rule.new(statement: r9, consequent: raw_material_flow_rate_delta_vis.tag, antecedent: [viscosity_error.tag])
-    rule10 = Rule.new(statement: r10, consequent: raw_material_flow_rate_delta_vis.tag, antecedent: [viscosity_error.tag])
-    rule11 = Rule.new(statement: r11, consequent: raw_material_flow_rate_delta_vis.tag, antecedent: [viscosity_error.tag])
-    rule12 = Rule.new(statement: r12, consequent: raw_material_flow_rate_delta_vis.tag, antecedent: [viscosity_error.tag])
-    rule13 = Rule.new(statement: r13, consequent: raw_material_flow_rate_delta_vis.tag, antecedent: [viscosity_error.tag])
-    rule14 = Rule.new(statement: r14, consequent: raw_material_flow_rate_delta_vis.tag, antecedent: [viscosity_error.tag])
-    rule15 = Rule.new(statement: r15, consequent: raw_material_flow_rate_delta_vis.tag, antecedent: [viscosity_error.tag])
-    rule16 = Rule.new(statement: r16, consequent: raw_material_flow_rate_delta_vis.tag, antecedent: [viscosity_error.tag])
-    rule17 = Rule.new(statement: r17, consequent: raw_material_flow_rate_delta_vis.tag, antecedent: [viscosity_error.tag])
-    rule18 = Rule.new(statement: r18, consequent: raw_material_flow_rate_delta_vis.tag, antecedent: [viscosity_error.tag])
-    rule19 = Rule.new(statement: r19, consequent: raw_material_flow_rate_delta_vis.tag, antecedent: [viscosity_error.tag])
-    rule20 = Rule.new(statement: r20, consequent: raw_material_flow_rate_delta_vis.tag, antecedent: [viscosity_error.tag])
-    rule21 = Rule.new(statement: r21, consequent: raw_material_flow_rate_delta_vis.tag, antecedent: [viscosity_error.tag])
-    rule22 = Rule.new(statement: r22, consequent: raw_material_flow_rate_delta_vis.tag, antecedent: [viscosity_error.tag])
-    rule23 = Rule.new(statement: r23, consequent: raw_material_flow_rate_delta_vis.tag, antecedent: [viscosity_error.tag])
-    rule24 = Rule.new(statement: r24, consequent: raw_material_flow_rate_delta_vis.tag, antecedent: [viscosity_error.tag])
-    rule25 = Rule.new(statement: r25, consequent: raw_material_flow_rate_delta_vis.tag, antecedent: [viscosity_error.tag])
 
-    rules = [rule1,rule2,rule3,rule4,rule5,
-            rule6,rule7,rule8,rule9,rule10,
-            rule11,rule12,rule13,rule14,rule15,
-            rule16,rule17,rule18,rule19,rule20,
-            rule21,rule22,rule23,rule24,rule25]
+    rules = [rule1,rule2,rule3,rule4,rule5]
 
     {:ok, srm_vis_pid} =
       System.start_link(antecedent: [viscosity_error], consequent: raw_material_flow_rate_delta_vis, rules: rules)
 
 
     # Water Flow Rate Delta Controller
-    r1 = fn [at1, at2, con] -> (at1 ~> "mb" &&& at2 ~> "mb") >>> con ~> "pb"
+    r1 = fn [at1, con] -> (at1 ~> "mb" &&& at1 ~> "mb") >>> con ~> "pb"
     end
-    r2 = fn [at1, at2, con] -> (at1 ~> "mb" &&& at2 ~> "ms") >>> con ~> "pb"
+    r2 = fn [at1, con] -> (at1 ~> "ms" &&& at1 ~> "ms") >>> con ~> "ps"
     end
-    r3 = fn [at1, at2, con] -> (at1 ~> "mb" &&& at2 ~> "ze") >>> con ~> "pb"
+    r3 = fn [at1, con] -> (at1 ~> "ze" &&& at1 ~> "ze") >>> con ~> "ze"
     end
-    r4 = fn [at1, at2, con] -> (at1 ~> "mb" &&& at2 ~> "ps") >>> con ~> "ps"
+    r4 = fn [at1, con] -> (at1 ~> "ps" &&& at1 ~> "ps") >>> con ~> "ms"
     end
-    r5 = fn [at1, at2, con] -> (at1 ~> "mb" &&& at2 ~> "pb") >>> con ~> "ps"
-    end
-    r6 = fn [at1, at2, con] -> (at1 ~> "ms" &&& at2 ~> "mb") >>> con ~> "pb"
-    end
-    r7 = fn [at1, at2, con] -> (at1 ~> "ms" &&& at2 ~> "ms") >>> con ~> "ps"
-    end
-    r8 = fn [at1, at2, con] -> (at1 ~> "ms" &&& at2 ~> "ze") >>> con ~> "ze"
-    end
-    r9 = fn [at1, at2, con] -> (at1 ~> "ms" &&& at2 ~> "ps") >>> con ~> "ms"
-    end
-    r10 = fn [at1, at2, con] -> (at1 ~> "ms" &&& at2 ~> "pb") >>> con ~> "ps"
-    end
-    r11 = fn [at1, at2, con] -> (at1 ~> "ze" &&& at2 ~> "mb") >>> con ~> "ps"
-    end
-    r12 = fn [at1, at2, con] -> (at1 ~> "ze" &&& at2 ~> "ms") >>> con ~> "ps"
-    end
-    r13 = fn [at1, at2, con] -> (at1 ~> "ze" &&& at2 ~> "ze") >>> con ~> "ze"
-    end
-    r14 = fn [at1, at2, con] -> (at1 ~> "ze" &&& at2 ~> "ps") >>> con ~> "ms"
-    end
-    r15 = fn [at1, at2, con] -> (at1 ~> "ze" &&& at2 ~> "pb") >>> con ~> "ms"
-    end
-    r16 = fn [at1, at2, con] -> (at1 ~> "ps" &&& at2 ~> "mb") >>> con ~> "ms"
-    end
-    r17 = fn [at1, at2, con] -> (at1 ~> "ps" &&& at2 ~> "ms") >>> con ~> "ps"
-    end
-    r18 = fn [at1, at2, con] -> (at1 ~> "ps" &&& at2 ~> "ze") >>> con ~> "ze"
-    end
-    r19 = fn [at1, at2, con] -> (at1 ~> "ps" &&& at2 ~> "ps") >>> con ~> "ms"
-    end
-    r20 = fn [at1, at2, con] -> (at1 ~> "ps" &&& at2 ~> "pb") >>> con ~> "mb"
-    end
-    r21 = fn [at1, at2, con] -> (at1 ~> "pb" &&& at2 ~> "mb") >>> con ~> "ms"
-    end
-    r22 = fn [at1, at2, con] -> (at1 ~> "pb" &&& at2 ~> "ms") >>> con ~> "ms"
-    end
-    r23 = fn [at1, at2, con] -> (at1 ~> "pb" &&& at2 ~> "ze") >>> con ~> "mb"
-    end
-    r24 = fn [at1, at2, con] -> (at1 ~> "pb" &&& at2 ~> "ps") >>> con ~> "mb"
-    end
-    r25 = fn [at1, at2, con] -> (at1 ~> "pb" &&& at2 ~> "pb") >>> con ~> "mb"
+    r5 = fn [at1, con] -> (at1 ~> "pb" &&& at1 ~> "pb") >>> con ~> "mb"
     end
 
     # Flow Rate Density Error
@@ -264,32 +136,8 @@ defmodule MandaniBTest do
     rule3 = Rule.new(statement: r3, consequent: water_flow_rate_delta_dens.tag, antecedent: [density_error.tag])
     rule4 = Rule.new(statement: r4, consequent: water_flow_rate_delta_dens.tag, antecedent: [density_error.tag])
     rule5 = Rule.new(statement: r5, consequent: water_flow_rate_delta_dens.tag, antecedent: [density_error.tag])
-    rule6 = Rule.new(statement: r6, consequent: water_flow_rate_delta_dens.tag, antecedent: [density_error.tag])
-    rule7 = Rule.new(statement: r7, consequent: water_flow_rate_delta_dens.tag, antecedent: [density_error.tag])
-    rule8 = Rule.new(statement: r8, consequent: water_flow_rate_delta_dens.tag, antecedent: [density_error.tag])
-    rule9 = Rule.new(statement: r9, consequent: water_flow_rate_delta_dens.tag, antecedent: [density_error.tag])
-    rule10 = Rule.new(statement: r10, consequent: water_flow_rate_delta_dens.tag, antecedent: [density_error.tag])
-    rule11 = Rule.new(statement: r11, consequent: water_flow_rate_delta_dens.tag, antecedent: [density_error.tag])
-    rule12 = Rule.new(statement: r12, consequent: water_flow_rate_delta_dens.tag, antecedent: [density_error.tag])
-    rule13 = Rule.new(statement: r13, consequent: water_flow_rate_delta_dens.tag, antecedent: [density_error.tag])
-    rule14 = Rule.new(statement: r14, consequent: water_flow_rate_delta_dens.tag, antecedent: [density_error.tag])
-    rule15 = Rule.new(statement: r15, consequent: water_flow_rate_delta_dens.tag, antecedent: [density_error.tag])
-    rule16 = Rule.new(statement: r16, consequent: water_flow_rate_delta_dens.tag, antecedent: [density_error.tag])
-    rule17 = Rule.new(statement: r17, consequent: water_flow_rate_delta_dens.tag, antecedent: [density_error.tag])
-    rule18 = Rule.new(statement: r18, consequent: water_flow_rate_delta_dens.tag, antecedent: [density_error.tag])
-    rule19 = Rule.new(statement: r19, consequent: water_flow_rate_delta_dens.tag, antecedent: [density_error.tag])
-    rule20 = Rule.new(statement: r20, consequent: water_flow_rate_delta_dens.tag, antecedent: [density_error.tag])
-    rule21 = Rule.new(statement: r21, consequent: water_flow_rate_delta_dens.tag, antecedent: [density_error.tag])
-    rule22 = Rule.new(statement: r22, consequent: water_flow_rate_delta_dens.tag, antecedent: [density_error.tag])
-    rule23 = Rule.new(statement: r23, consequent: water_flow_rate_delta_dens.tag, antecedent: [density_error.tag])
-    rule24 = Rule.new(statement: r24, consequent: water_flow_rate_delta_dens.tag, antecedent: [density_error.tag])
-    rule25 = Rule.new(statement: r25, consequent: water_flow_rate_delta_dens.tag, antecedent: [density_error.tag])
 
-    rules = [rule1,rule2,rule3,rule4,rule5,
-            rule6,rule7,rule8,rule9,rule10,
-            rule11,rule12,rule13,rule14,rule15,
-            rule16,rule17,rule18,rule19,rule20,
-            rule21,rule22,rule23,rule24,rule25]
+    rules = [rule1,rule2,rule3,rule4,rule5]
 
     {:ok, sw_dens_pid} =
       System.start_link(antecedent: [density_error], consequent: water_flow_rate_delta_dens, rules: rules)
@@ -300,32 +148,8 @@ defmodule MandaniBTest do
     rule3 = Rule.new(statement: r3, consequent: water_flow_rate_delta_vis.tag, antecedent: [viscosity_error.tag])
     rule4 = Rule.new(statement: r4, consequent: water_flow_rate_delta_vis.tag, antecedent: [viscosity_error.tag])
     rule5 = Rule.new(statement: r5, consequent: water_flow_rate_delta_vis.tag, antecedent: [viscosity_error.tag])
-    rule6 = Rule.new(statement: r6, consequent: water_flow_rate_delta_vis.tag, antecedent: [viscosity_error.tag])
-    rule7 = Rule.new(statement: r7, consequent: water_flow_rate_delta_vis.tag, antecedent: [viscosity_error.tag])
-    rule8 = Rule.new(statement: r8, consequent: water_flow_rate_delta_vis.tag, antecedent: [viscosity_error.tag])
-    rule9 = Rule.new(statement: r9, consequent: water_flow_rate_delta_vis.tag, antecedent: [viscosity_error.tag])
-    rule10 = Rule.new(statement: r10, consequent: water_flow_rate_delta_vis.tag, antecedent: [viscosity_error.tag])
-    rule11 = Rule.new(statement: r11, consequent: water_flow_rate_delta_vis.tag, antecedent: [viscosity_error.tag])
-    rule12 = Rule.new(statement: r12, consequent: water_flow_rate_delta_vis.tag, antecedent: [viscosity_error.tag])
-    rule13 = Rule.new(statement: r13, consequent: water_flow_rate_delta_vis.tag, antecedent: [viscosity_error.tag])
-    rule14 = Rule.new(statement: r14, consequent: water_flow_rate_delta_vis.tag, antecedent: [viscosity_error.tag])
-    rule15 = Rule.new(statement: r15, consequent: water_flow_rate_delta_vis.tag, antecedent: [viscosity_error.tag])
-    rule16 = Rule.new(statement: r16, consequent: water_flow_rate_delta_vis.tag, antecedent: [viscosity_error.tag])
-    rule17 = Rule.new(statement: r17, consequent: water_flow_rate_delta_vis.tag, antecedent: [viscosity_error.tag])
-    rule18 = Rule.new(statement: r18, consequent: water_flow_rate_delta_vis.tag, antecedent: [viscosity_error.tag])
-    rule19 = Rule.new(statement: r19, consequent: water_flow_rate_delta_vis.tag, antecedent: [viscosity_error.tag])
-    rule20 = Rule.new(statement: r20, consequent: water_flow_rate_delta_vis.tag, antecedent: [viscosity_error.tag])
-    rule21 = Rule.new(statement: r21, consequent: water_flow_rate_delta_vis.tag, antecedent: [viscosity_error.tag])
-    rule22 = Rule.new(statement: r22, consequent: water_flow_rate_delta_vis.tag, antecedent: [viscosity_error.tag])
-    rule23 = Rule.new(statement: r23, consequent: water_flow_rate_delta_vis.tag, antecedent: [viscosity_error.tag])
-    rule24 = Rule.new(statement: r24, consequent: water_flow_rate_delta_vis.tag, antecedent: [viscosity_error.tag])
-    rule25 = Rule.new(statement: r25, consequent: water_flow_rate_delta_vis.tag, antecedent: [viscosity_error.tag])
 
-    rules = [rule1,rule2,rule3,rule4,rule5,
-            rule6,rule7,rule8,rule9,rule10,
-            rule11,rule12,rule13,rule14,rule15,
-            rule16,rule17,rule18,rule19,rule20,
-            rule21,rule22,rule23,rule24,rule25]
+    rules = [rule1,rule2,rule3,rule4,rule5]
 
     {:ok, sw_vis_pid} =
       System.start_link(antecedent: [viscosity_error], consequent: water_flow_rate_delta_vis, rules: rules)
@@ -386,18 +210,18 @@ defmodule MandaniBTest do
 #     compute({srm_res_pid, srm_vis_pid, sw_dens_pid, sw_vis_pid}, [-2.04, -0.04, -10.6])
 #     compute({srm_res_pid, srm_vis_pid, sw_dens_pid, sw_vis_pid}, [-2.53, -0.04, -18.9])
   end
-#
-#   # defp random_between(min, max), do: (min + ((max - min) * :rand.uniform())) |> Float.round(3)
-#
+
+  # defp random_between(min, max), do: (min + ((max - min) * :rand.uniform())) |> Float.round(3)
+
   defp compute({srm_res_pid, srm_vis_pid, sw_dens_pid, sw_vis_pid},[residual_error, density_error, viscosity_error]) do
-    # output = System.compute(srm_res_pid, [4.1])
-    srm_res_output = System.compute(srm_res_pid, [residual_error])
+    System.compute(srm_res_pid, [4.1])
+    # srm_res_output = System.compute(srm_res_pid, [residual_error])
     # srm_vis_output = System.compute(srm_vis_pid, [viscosity_error])
     # sw_dens_output = System.compute(sw_dens_pid, [density_error])
     # sw_vis_output = System.compute(sw_vis_pid, [viscosity_error])
 
     IO.puts("Hello World")
     # #{inspect({[residual_error]})} => #{inspect({srm_res_output})}
-    
+
   end
 end
